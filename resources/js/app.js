b40760c597109c54c67618f0d9fdf048d2fe0ca5
Kamil <kamil.piech.97@gmail.com>
2023-01-08 20:29:15 +0100

    #18 - added chart with possible installments sizes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import '../css/app.css'
 import {createApp, h} from 'vue'
 import {createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3'
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import VueApexCharts from "vue3-apexcharts";
+
 createInertiaApp({
     resolve:  (name) => {
         const page = resolvePageComponent(
@@ -14,6 +16,7 @@ createInertiaApp({
     setup({el, app, props, plugin}) {
         createApp({render: () => h(app, props)})
             .use(plugin)
+            .use(VueApexCharts)
             .component('InertiaHead', Head)
             .component('InertiaLink', Link)
             .mount(el)
